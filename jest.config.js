e9ad49fbf55d783f2ad7a196ae3e2f30b64bba56
Gaurav Singh <108992402+gv-sh@users.noreply.github.com>
2025-03-14 15:51:06 +1000

    update tests

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,12 @@
 // jest.config.js
 module.exports = {
     testEnvironment: 'node',
-    testTimeout: 10000,
-    // We need to tell Jest to ignore the node_modules folder
+    testTimeout: 100000,
     testPathIgnorePatterns: ['/node_modules/'],
-    // Run tests in parallel to speed up execution
-    maxWorkers: '50%',
-    // Add verbose output
+    maxWorkers: '25%',
     verbose: true,
-    // Let's create a coverage report
     collectCoverage: true,
     coverageDirectory: './coverage',
     coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
-    // We'll use our own reporters
     reporters: ['default']
   };
